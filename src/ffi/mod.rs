e8c91a6b6c35325e8c5ec4cdafb5577db3157a90
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-09-13 18:56:43 -0400

    Adds `MountPoint::{is_root, is_home, contains}`

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -5,5 +5,5 @@ mod time_fmt;
 
 pub use home::get_home_dir;
 pub use lstat::Lstat;
-pub use mount_points::probe_mount_points;
+pub use mount_points::{probe_mount_points, MountPoint};
 pub use time_fmt::format_time;
