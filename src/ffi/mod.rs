cb42b28ad0fe35661a934fbc6c89eda174ac9642
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-09-13 19:28:14 -0400

    Add `ffi::real_user_id`

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -2,8 +2,10 @@ mod home;
 mod lstat;
 mod mount_points;
 mod time_fmt;
+mod user;
 
 pub use home::get_home_dir;
 pub use lstat::Lstat;
 pub use mount_points::{probe_mount_points, MountPoint};
 pub use time_fmt::format_time;
+pub use user::{effective_user_id, real_user_id};
